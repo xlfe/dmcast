8a864394369fbd491baf2a8d7e189c877a9ca59c
xlfe <felixb@gmail.com>
2022-04-24 22:07:46 +1000

    first release

--- main.py
+++ main.py
@@ -39,9 +39,6 @@ def make_msg(ns=None):
     return msg
 
 
-PLAYING_ACTION = 0
-PLAY_NEW = 1
-
 class Chromecast():
 
     def __init__(self, host, port):
@@ -49,8 +46,8 @@ class Chromecast():
         self.port = port
         self.running = True
         self.id_counts = collections.Counter()
-        self.desired_actions = []
-        self.desired_media = None
+        self.dmr_session = None
+
         self.dispatch = {
                 'RECEIVER_STATUS': self.handle_receiver_status,
                 'MEDIA_STATUS': self.handle_media_status,
@@ -59,55 +56,48 @@ class Chromecast():
 
     async def handle_media_status(self, data):
 
-        first_idx = None
-        for idx, a in enumerate(self.desired_actions):
-            if a[0] == PLAYING_ACTION:
-                first_idx = idx
-                break
-        if first_idx is not None:
-
-            _, action = self.desired_actions.pop(first_idx)
-            msid = data['status'][0]['mediaSessionId']
-            await action(msid, self.session)
-        else:
-            await self.state(data)
-
-    async def state(self, data):
-        print(json.dumps(data, indent=2))
+        if data['status']:
+            self.msid = data['status'][0]['mediaSessionId']
 
     async def handle_receiver_status(self, msg):
 
+        state = {'namespace': 'receiver',
+                 'volume_level': msg['status']['volume']['level'],
+                 'volume_muted': msg['status']['volume']['muted']}
+
         if 'applications' in msg['status']:
+
+            apps = msg['status']['applications']
+            state['current_app'] = apps[0]['displayName']
+
             try:
-                da = min(filter(lambda a: a['appId'] == DEFAULT_APP, msg['status']['applications']))
-                self.session = da['sessionId']
-                print(f'DEFAULT App running, connecting now {self.session}')
-                await self.connect(dst=self.session)
+                # find details for DMR if it is already running
+                da = min(filter(lambda a: a['appId'] == DEFAULT_APP, apps))
+                self.dmr_session = da['sessionId']
 
-                if self.desired_media:
-                    await self.get_status(dst=self.session)
-                    await self.play_media(self.desired_media)
-                    self.desired_media = None
-                else:
-                    await self.media_status()
+                # open a connection
+                await self.connect(dst=self.dmr_session)
 
-                await self.ping()
+                # check if anything is playing
+                await self.media_status()
+
             except ValueError:
-
-                print('DEFAULT App not running')
-                if self.desired_media:
-                    print('Starting default app')
-                    await self.launch_app()
-                    await self.get_status()
-                    await self.ping()
-
+                self.dmr_session = None
         else:
-            await self.state({'playing': False})
+            self.dmr_session = None
+
+        await self.notify_state(state)
+
+    async def notify_state(self, state):
+        pass
 
     async def recv_loop(self):
 
         while self.running:
             data = await self.reader.read(4)
+
+            if len(data) != 4:
+                continue
             read_len = unpack(">I", data)[0]
 
             data = await self.reader.read(read_len)
@@ -115,16 +105,15 @@ class Chromecast():
             response = make_msg()
             response.ParseFromString(data)
             resp = json.loads(response.payload_utf8)
-            print(response.namespace)
-            print(json.dumps(resp, indent=2))
+            # print(response.namespace)
+            # print(json.dumps(resp, indent=2))
             handler = self.dispatch.get(resp['type'])
             if handler:
                 await handler(resp)
 
-        self.reader.close()
-
     async def start(self):
-        self.reader, self.writer = await asyncio.open_connection(self.host, self.port, ssl=ssl.SSLContext())
+        self.reader, self.writer = \
+            await asyncio.open_connection(self.host, self.port, ssl=ssl.SSLContext())
 
         # Connect and get-status
         await self.connect()
@@ -133,13 +122,18 @@ class Chromecast():
 
         self.read_task = asyncio.create_task(self.recv_loop())
 
-        while True:
+        while self.running:
             await asyncio.sleep(1)
+            await self.stop()
 
-    async def send_messager(self, ns, data, dst=None):
-        self.id_counts[dst] += 1
-        data['requestId'] = self.id_counts[dst]
-        await self.send_message(ns, data, dst)
+    async def stop(self):
+        if self.dmr_session:
+            await self.stop_drm()
+        await self.disconnect()
+
+        self.running = False
+        self.writer.close()
+        await self.writer.wait_closed()
 
     async def send_message(self, ns, data, dst=None):
 
@@ -152,92 +146,81 @@ class Chromecast():
         self.writer.write(sz + msg.SerializeToString())
         await self.writer.drain()
 
+    async def send_messager(self, ns, data, dst=None):
+        self.id_counts[dst] += 1
+        data['requestId'] = self.id_counts[dst]
+        await self.send_message(ns, data, dst)
+
+    async def dmr_message(self, data, ns='media'):
+        assert self.dmr_session
+        await self.send_messager(ns, data, dst=self.dmr_session)
+
     async def ping(self):
         await self.send_message('heartbeat', {"type": "PING"})
 
     async def pong(self, _):
         await self.send_message('heartbeat', {"type": "PONG"})
 
-    async def connect(self, dst=None):
-        await self.send_message('con', {"type": "CONNECT",
-                                        "origin": {},
-                                        "userAgent": "dmcast",
-                                        "senderInfo": {}}, dst)
-
     async def get_status(self, dst=None):
         await self.send_messager('receiver', {"type": "GET_STATUS"}, dst)
 
-    async def launch_app(self):
-        await self.send_messager('receiver',
-                                 {"type": "LAUNCH", "appId": DEFAULT_APP})
-
-    async def media_status(self):
-        await self.send_messager('media', {"type": "GET_STATUS"}, self.session)
-
-    async def media_play(self, msid):
-        await self.send_messager('media',
-                                 {"type": "PLAY",
-                                  "mediaSessionId": msid},
-                                 self.session)
-
-    async def media_pause(self, msid):
-        await self.send_messager('media',
-                                 {"type": "PAUSE",
-                                  'mediaSessionId': msid},
-                                 self.session)
-
-    async def media_stop(self, msid):
-        await self.send_messager('media',
-                                 {"type": "STOP",
-                                  'mediaSessionId': msid},
-                                 self.session)
-
-    async def app_stop(self):
-        await self.send_message('receiver',
-                                {"type": "STOP"},
-                                self.session)
-        self.session = None
-
-    async def set_volume(self, volume):
-        volume = min(max(0, volume), 1)
-        await self.send_messager('receiver',
-                                 {"type": "SET_VOLUME",
-                                  "volume": {"level": volume}},
-                                 self.session)
-
-    async def set_volume_muted(self, muted):
-        await self.send_messager('receiver',
-                                 {"type": "SET_VOLUME",
-                                  "volume": {"muted": muted}},
-                                 self.session)
-
-    async def disconnect(self, dst):
+    async def disconnect(self, dst=None):
         await self.send_message('con', {"type": "CLOSE", "origin": {}}, dst)
 
-    async def play_media(self, media):
+    async def connect(self, dst=None):
+        await self.send_message('con', {"type": "CONNECT", "origin": {},
+                                        "userAgent": "dmcast",
+                                        "senderInfo": {}}, dst)
+
+    async def launch_dmr(self):
+        if self.dmr_session is None:
+            await self.send_messager('receiver', {"type": "LAUNCH", "appId": DEFAULT_APP})
+            await self.get_status()
+
+    async def stop_drm(self):
+        await self.dmr_message({"type": "STOP"}, 'receiver')
+        self.dmr_session = None
+
+    # Media
+
+    async def media_load(self, media):
+        assert self.dmr_session
 
         PLAY = {
             "type": "LOAD",
-            "sessionId": self.session,
+            "sessionId": self.dmr_session,
             "autoplay": True,
             "customData": {},
             "media": media}
-        await self.send_messager('media', PLAY, dst=self.session)
+        await self.dmr_message('media', PLAY)
+
+    async def media_status(self):
+        await self.dmr_message({"type": "GET_STATUS"})
+
+    async def media_play(self, msid):
+        await self.dmr_message({"type": "PLAY", "mediaSessionId": msid})
+
+    async def media_pause(self, msid):
+        await self.dmr_message({"type": "PAUSE", 'mediaSessionId': msid})
+
+    async def media_stop(self, msid):
+        await self.dmr_message({"type": "STOP", 'mediaSessionId': msid})
+
+    # Device
+
+    async def device_volume(self, volume):
+        v = min(max(0, volume), 1)
+        await self.send_messager('receiver', {"type": "SET_VOLUME", "volume": {"level": v}})
+
+    async def device_mute(self, muted):
+        await self.send_messager('receiver', {"type": "SET_VOLUME", "volume": {"muted": muted}})
 
 
-# c.desired_media = media
-# c.desired_actions = [[PLAYING_ACTION, c.stop]]
 
-# Chromecast states
-# DMR not running
-#  Get notified of anything running
-
-# DMR running (session reflects this connection)
-# Play,pause, stop media
-# mute / volume
-
-# Play media at specified volume
-
-
-
-
+# example media
+# media = {
+    # "contentId": "https://your-domain.com/hello.mp3",
+    # "streamType": "BUFFERED",
+    # "contentType": "audio/mp3",
+    # "metadata": {}
+# }
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dmcast',
+    packages=['dmcast'],
+    version='0.0.1',
+    license='EPL',
+    description='simple chromecast utility',
+    url='https://github.com/xlfe/dmcast',
+    keywords=[
+        'python', 'chromecast', 'internet-of-things',
+        'chromecast-audio', 'cast'],
+    install_requires=[
+        'protobuf>=2.6.0'
+    ],
+    classifiers=[
+     'Development Status :: 3 - Alpha',
+     'Intended Audience :: Developers',
+     'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
+     'Programming Language :: Python :: 3.9',
+    ]
+)
+
